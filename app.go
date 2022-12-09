56fa1f5ccaede65da851b39faf627304d4c6631e
RaulSantosCalvo <raul.santos.calvo@gmail.com>
2022-12-08 20:05:50 -0500

    update lib ver

--- app.go
+++ app.go
@@ -9,11 +9,28 @@ import (
 
 const accountId string = "ad27e265-9605-4b4b-a0e5-3003ea9cc4dc"
 
-var account string = fmt.Sprintf(`{"attributes":{"bank_id":"123456","bank_id_code":"GBDSC","base_currency":"GBP","bic":"EXMPLGB2XXX","country":"GB","name":["test_account"]},"id":"%s","organisation_id":"eb0bd6f5-c3f5-44b2-b677-acd23cdde73c","type":"accounts"}`, accountId)
+// returns mocked account object
+func getMockAccount(id string) accountapi.AccountData {
+	country := "GB"
+	account := accountapi.AccountData{
+		ID:             id,
+		OrganisationID: "eb0bd6f5-c3f5-44b2-b677-acd23cdde73c",
+		Type:           "accounts",
+		Attributes: &accountapi.AccountAttributes{
+			Country:      &country,
+			BaseCurrency: "GBP",
+			BankID:       "123456",
+			BankIDCode:   "GBDSC",
+			Bic:          "EXMPLGB2XXX",
+			Name:         []string{"Test", "Account"},
+		},
+	}
+	return account
+}
 
 func create() {
 	fmt.Println("create")
-	result, err := accountapi.Create(account)
+	result, err := accountapi.Create(getMockAccount(accountId))
 	if err != nil {
 		log.Fatal("error: ", err)
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/RaulSantosCalvo/interview-accountapi-app
 
 go 1.19
 
-require github.com/RaulSantosCalvo/interview-accountapi v0.1.3
+require github.com/RaulSantosCalvo/interview-accountapi v0.2.1
